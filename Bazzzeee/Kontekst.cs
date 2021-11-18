0cef5d2680fcf0f10d9e08a92cf888241726218d
Dragan Pepic <nedimco2006@gmail.com>
2021-11-18 10:28:27 +0100

    Add project files.

---
+++ Bazzzeee/Kontekst.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bazzzeee
+{
+    public class Kontekst : DbContext
+    {
+        public DbSet<Osoba> Osobas { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog = BazaZaEF;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Osoba>().HasKey(o => o.Id);
+            Osoba o = new();
+            modelBuilder.Entity<Osoba>().Property(nameof(o.Ime)).IsRequired();
+            modelBuilder.Entity<Osoba>().Property(nameof(o.Prezime)).IsRequired();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Bazzzeee/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bazzzeee
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Kontekst _kontekst = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+            _kontekst.Database.EnsureCreated();
+            DatagridOsoba.ItemsSource = _kontekst.Osobas.Local.ToObservableCollection();
+
+            _kontekst.Osobas.ToList();
+
+         
+        }
+
+        private void Dodaj(object sender, RoutedEventArgs e)
+        {
+            _kontekst.Add(new Osoba { Ime = "Pera", Prezime = "Peric" });
+            _kontekst.SaveChanges();
+        }
+
+        private void Brisi(object sender, RoutedEventArgs e)
+        {
+            if (DatagridOsoba.SelectedItem is not null)
+            {
+                _kontekst.Remove(DatagridOsoba.SelectedItem as Osoba);
+                _kontekst.SaveChanges();
+            }
+        }
+    }
+}
